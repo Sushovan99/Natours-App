28a738746f82895409156a9aa1c62c52974ce967
Sushovan Biswas <biswassushovan99@gmail.com>
2022-06-02 16:44:38 +0530

    Add some basic routing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const morgan = require('morgan');
 
 dotenv.config({
   path: `${__dirname}/config.env`,
@@ -8,8 +10,74 @@ dotenv.config({
 const PORT = process.env.PORT;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express');
+// Reading tours-simple.json
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+// Body parser middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Get all tours
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({ status: 'success', data: { tours } });
+});
+
+// Get single tour
+app.get('/api/v1/tours/:id', (req, res) => {
+  const ID = parseInt(req.params.id, 10);
+  const tour = tours.find((el) => el.id === ID);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Tour not found' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+// Create a tour
+app.post('/api/v1/tours', (req, res) => {
+  const newID = tours[tours.length - 1].id + 1;
+  const body = req.body;
+  const newTour = { id: newID, ...body };
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) console.log(err);
+      console.log('File written successfully');
+    }
+  );
+  res.status(201).json({ status: 'success', data: { newTour } });
+});
+
+// Update a tour
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length - 1) {
+    return res.status(404).json({ status: 'fail', message: 'Not found' });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here>',
+    },
+  });
+});
+
+// Delete a tour
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length - 1) {
+    return res.status(404).json({ status: 'fail', message: 'Not found' });
+  }
+  res.status(204).json({ status: 'success', data: null });
 });
 
 app.listen(PORT, () => {
