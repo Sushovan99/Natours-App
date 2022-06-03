10587dcb8b1447114fe04c732334c4c7680e0e2e
Sushovan Biswas <biswassushovan99@gmail.com>
2022-06-03 08:32:22 +0530

    Connect our DB

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({
   path: './config.env',
 });
 
+// Connecting DB
+const DB = process.env.DB_CONNECT.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connected'));
+
 const app = require('./app');
 
-console.log(process.env.NODE_ENV);
+// Starting server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
