0a7727317312ddda94b201ae2aa25af454c32a9e
Sushovan Biswas <biswassushovan99@gmail.com>
2022-06-16 11:03:52 +0530

    Add 404 Not found errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,9 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
 const catchAsyncErr = require('../utils/catchAsyncError');
 
-exports.aliasTopTours = (req, res, next) => {
+exports.aliasTopTours = (req, _res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,ratingsAverage,price,summary,difficulty';
@@ -10,6 +11,7 @@ exports.aliasTopTours = (req, res, next) => {
 };
 
 //--->> Tour Controllers/Handler functions
+// Wrapping our handlers with catchAsyncErr() to handle error separately
 exports.getAllTours = catchAsyncErr(async (req, res, _next) => {
   // BUILD THE QUERY
   const features = new APIFeatures(Tour.find(), req.query)
@@ -28,9 +30,14 @@ exports.getAllTours = catchAsyncErr(async (req, res, _next) => {
   });
 });
 
-exports.getTour = catchAsyncErr(async (req, res, _next) => {
+exports.getTour = catchAsyncErr(async (req, res, next) => {
   // .findById() is the same as writing Tour.findOne({_id: req.params.id})
   const tour = await Tour.findById(req.params.id);
+
+  // Handling 404 error
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -50,13 +57,18 @@ exports.createTour = catchAsyncErr(async (req, res, _next) => {
   });
 });
 
-exports.updateTour = catchAsyncErr(async (req, res, _next) => {
+exports.updateTour = catchAsyncErr(async (req, res, next) => {
   const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
     // The new: true option returns the updated document
     new: true,
     // runValidators: if true, runs schema validation before updating the document. If not provided then validation will not be performed.
     runValidators: true,
   });
+
+  // Handling 404 error
+  if (!updatedTour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
 
   res.status(200).json({
     status: 'success',
@@ -66,8 +78,11 @@ exports.updateTour = catchAsyncErr(async (req, res, _next) => {
   });
 });
 
-exports.deleteTour = catchAsyncErr(async (req, res, _next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+exports.deleteTour = catchAsyncErr(async (req, res, next) => {
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(204).json({ status: 'success', data: null });
 });
 
--- utils/catchAsyncError.js
+++ utils/catchAsyncError.js
@@ -1,1 +1,3 @@
-module.exports = (fn) => (req, res, next) => fn(req, res, next).catch(next);
+// Since, all the handlers are async functions it returns a promise se we can use .catch() method
+module.exports = (fn) => (req, res, next) =>
+  fn(req, res, next).catch((err) => next(err));
